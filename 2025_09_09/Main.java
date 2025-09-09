5fc8a89d0261e4077d92d3c275077d1d55ff894f
Mông Đức Hiếu <mdhieu2006@gmail.com>
2025-09-09 15:03:55 +0700

    Add files via upload

---
+++ 2025_09_09/Main.java
@@ -0,0 +1,10 @@
+package qlmonhoc;
+
+import qlsinhvien.SinhVien;   // import class từ package khác
+
+public class MainMonHoc {
+    public static void main(String[] args) {
+        SinhVien sv = new SinhVien("Tran Thi B");
+        sv.hienThi();
+    }
+}
